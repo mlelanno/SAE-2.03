a88ee249f3a9fa16eb8b3a5486c79660b7065d21
Metehan Erdogan <metehan.erdogan@uha.fr>
2026-05-21 15:43:17 +0200

    Added the base.html layout and Bootstrap navigation

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # <-- N'oublie pas d'importer 'include'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # On indique à Django de router toutes les requêtes vers les URLs de notre app 'ludotheque'
+    path('', include('ludotheque.urls')), 
 ]
--- ludotheque/views.py
+++ ludotheque/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def accueil(request):
+    return render(request, 'ludotheque/index.html')
